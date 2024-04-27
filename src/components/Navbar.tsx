f5097e4c7474bdcdaff3cac2b9cb3d5cfa895edc
manjotdevs <manjotdevs459@gmail.com>
2024-04-27 20:27:15 +0530

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <div>
+        <h1>MS Production</h1>
+      </div>
+
+      <div>
+        
+      </div>
+    </div>
+  );
+}
