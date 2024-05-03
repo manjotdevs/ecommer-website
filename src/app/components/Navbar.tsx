a0dbbfcab5dd31aaea16b6460a0d0480265a1ae7
manjotdevs <manjotdevs459@gmail.com>
2024-05-03 23:55:01 +0530

    new navbar is up and working

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,56 @@
+'use client'
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav
+      className={`bg-black rounded-2xl fixed top-0 left-0 w-full z-50 transition-all duration-300 ease-in-out ${
+        isScrolled ? 'bg-black shadow' : 'bg-transparent'
+      }`}
+    >
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            {/* Mobile menu button */}
+          </div>
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex-shrink-0">
+              <Link href="/" className="font-bold text-2xl text-white">MS Production</Link>
+            </div>
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex space-x-4">
+                <Link href="/products" className="text-white hover:underline-offset-8">Products</Link>
+                <Link href="/" className="text-white hover:underline-offset-8">Products</Link>
+                <Link href="/" className="text-white hover:underline-offset-8">Products</Link>
+              </div>
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            {/* User menu */}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/app/products/page.tsx
@@ -0,0 +1,6 @@
+
+export default function products(){
+    return(
+        <h1 className="text-white">products</h1>
+    );
+}
